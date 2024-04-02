8d484dda9e0150c32d0c2f614e341bd202a9261a
ulypse <pgastong03@gmail.com>
2024-04-02 14:31:10 +0000

    Authentication bloc implementation

---
+++ lib/features/authentification/presentation/app/bloc/authentication_state.dart
@@ -0,0 +1,43 @@
+part of 'authentication_bloc.dart';
+
+sealed class AuthenticationState extends Equatable {
+  const AuthenticationState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class AuthenticationInitial extends AuthenticationState {}
+
+final class AuthLoadingState extends AuthenticationState {}
+
+final class UserLoadedState extends AuthenticationState {
+  const UserLoadedState({required this.user});
+
+  final UserEntity user;
+
+  @override
+  List<Object> get props => [user];
+}
+
+final class UserCreatedState extends AuthenticationState {}
+
+final class UserSignedInState extends AuthenticationState {
+  const UserSignedInState({required this.user});
+
+  final UserEntity user;
+
+  @override
+  List<Object> get props => [user];
+}
+
+final class UserSignedOutState extends AuthenticationState {}
+
+final class UserExistenceCheckedState extends AuthenticationState {
+  const UserExistenceCheckedState({required this.exists});
+
+  final bool exists;
+
+  @override
+  List<Object> get props => [exists];
+}
