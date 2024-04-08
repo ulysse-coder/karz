3bf000ce5f67c3e3ebf5b406df5feac4970d9987
gaston03 <pgastong03@gmail.com>
2024-04-08 17:23:41 +0000

    Implement reservation data layer -  Update parking and place models with json serialization  * Fix `ParkingModel` to parse `registeredAt` as string and convert to `DateTime` in constructor * update `_$ParkingModelToJson` to return `registeredAt` as string * Add `fromJson` and `toJson` methods and `empty` factory to `PlaceModel`

--- lib/features/parking/data/models/place_model.dart
+++ lib/features/parking/data/models/place_model.dart
@@ -1,9 +1,19 @@
 
+import 'package:json_annotation/json_annotation.dart';
 import 'package:ulysse_app/features/parking/domain/entities/place_entity.dart';
 
+part 'place_model.g.dart';
+
+@JsonSerializable()
 class PlaceModel extends PlaceEntity {
   const PlaceModel({
     required super.id,
     required super.name
   });
+
+  factory PlaceModel.fromJson(Map<String, dynamic> json) => _$PlaceModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PlaceModelToJson(this);
+
+  factory PlaceModel.empty() => const PlaceModel(id: '', name: '');
 }
