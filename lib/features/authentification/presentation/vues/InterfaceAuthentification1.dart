abcd7a6fe05cfad696e854c57610a3560ac99e66
MalickMohamedBeka <mohamedbekamalick21@gmail.com>
2024-04-12 06:52:37 +0000

    1ère interfac auhen

--- lib/features/authentification/presentation/vues/InterfaceAuthentification1.dart
+++ lib/features/authentification/presentation/vues/InterfaceAuthentification1.dart
@@ -1,165 +1,141 @@
-code interface authentification Google/Fac...
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class InterfaceAuthentification1 extends StatefulWidget {
+  const InterfaceAuthentification1({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
+  @override
+  State<InterfaceAuthentification1> createState() => _InterfaceState();
+}
+
+class _InterfaceState extends State<InterfaceAuthentification1> {
   @override
   Widget build(BuildContext context) {
+    int largeurEcran = MediaQuery.of(context).size.width.floor();
+    int longueurEcran = MediaQuery.of(context).size.height.floor();
+    debugPrint("Largeur: $largeurEcran");
+    debugPrint("Longueur: $longueurEcran");
+
     return MaterialApp(
-      title: 'Interface Authentif',
-      theme: ThemeData(),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(height: 15), // Remontée de l'image
-          Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('Bureau/interf.png'),
-                fit: BoxFit.cover,
+      home: Scaffold(
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: (longueurEcran / 41)),
+          height: double.maxFinite,
+          decoration: const BoxDecoration(
+            color: Color(0xff14213D),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'asset/images/Coder.png', // Chemin de l'image
+                fit: BoxFit.cover, // Ajuster l'image pour couvrir la zone
+                height: (longueurEcran / 2.0867),
               ),
-            ),
-          ),
-          SizedBox(height: 15), // Espacement réduit entre le texte et les boutons
-          Text(
-            'Charger votre image en PNG, JPG...',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'calibri'),
-          ),
-          Expanded(
-            child: Container(
-              color: Color(0xFF31B51C),
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(height: 5), // Espacement réduit entre les boutons
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 12), // Marge de 1 cm entre les boutons
-                    child: TextButton(
-                      onPressed: () {
-                        print('Connexion avec Google');
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                        minimumSize: MaterialStateProperty.all<Size>(Size(200, 50)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.android,
-                            color: Colors.red, // Couleur vive
-                            size: 30, // Taille de l'icône
-                          ), // Icône Google
-                          SizedBox(width: 10),
-                          Text(
-                            'Connexion avec Google',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Inter',
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 12), // Marge de 1 cm entre les boutons
-                    child: TextButton(
-                      onPressed: () {
-                        // Événement lorsque le bouton de connexion avec Facebook est pressé
-                        print('Connexion avec Facebook');
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                        minimumSize: MaterialStateProperty.all<Size>(Size(200, 50)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.facebook,
-                            color: Colors.blueAccent, // Couleur vive
-                            size: 30, // Taille de l'icône
-                          ), // Icône Facebook
-                          SizedBox(width: 10),
-                          Text(
-                            'Connexion avec Facebook',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 12), // Marge de 1 cm entre les boutons
-                    child: TextButton(
-                      onPressed: () {
-                        // Événement lorsque le bouton de connexion avec Email est pressé
-                        print('Connexion avec Email');
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                        minimumSize: MaterialStateProperty.all<Size>(Size(200, 50)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.email,
-                            color: Colors.black, // Couleur vive
-                            size: 30, // Taille de l'icône
-                          ), // Icône Email
-                          SizedBox(width: 10),
-                          Text(
-                            'Connexion avec Email',
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                ],
+              Text(
+                'Connectez vous à My app',
+                style: TextStyle(
+                  fontSize: (longueurEcran / 16.52),
+                  color: Colors.white,
+                  //fontFamily: 'Italianno',
+                ),
               ),
-            ),
+
+              SizedBox(height: 10),
+
+              //boutton 1
+              TextButton(
+                onPressed: () {
+                  debugPrint("Bouton Connexion avec Google appuyé");
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'asset/images/Coder.png', // Chemin de l'image
+                      //fit: BoxFit.cover, // Ajuster l'image pour couvrir la zone
+                      height: 20,
+                      width:  20,
+                    ),
+                    const Text(
+                      'Connexion avec Google',
+                      style: TextStyle(
+                        fontSize: 7,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  minimumSize: (Size(50, 50)),
+                ),
+              ),
+
+              SizedBox(height: 15),
+              //boutton2
+              TextButton(
+                onPressed: () {
+                  debugPrint("Bouton Connexion avec Facebook appuyé");
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'asset/images/Coder.png', // Chemin de l'image
+                      //fit: BoxFit.cover, // Ajuster l'image pour couvrir la zone
+                      height: 20,
+                      width:  20,
+                    ),
+                    const Text(
+                      'Connexion avec Facebook',
+                      style: TextStyle(
+                        fontSize: 7,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  minimumSize: (Size(50, 50)),
+                ),
+              ),
+              SizedBox(height: 15),
+              //boutton3
+              TextButton(
+                onPressed: () {
+                  debugPrint("Bouton Connexion avec Email appuyé");
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'asset/images/Coder.png', // Chemin de l'image
+                      //fit: BoxFit.cover, // Ajuster l'image pour couvrir la zone
+                      height: 20,
+                      width:  20,
+                    ),
+                    const Text(
+                      'Connexion avec Email',
+                      style: TextStyle(
+                        fontSize: 7,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  minimumSize: (Size(50, 50)),
+                ),
+              ),
+            ]
           ),
-        ],
+        ),
       ),
     );
   }
 }
-
-
-
-
